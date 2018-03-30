abc61c08f590af42697656f459eb50b3bb2bcde6
Madeline Stark <madeline.stark1@gmail.com>
2018-03-30 23:20:51 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,8 +17,8 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, data_hash|
     data_hash.each do |key, value|
-      binding.pry
       if key == :favorite_ice_cream_flavors
+        binding.pry
         if favorite_ice_cream_flavors.include?("strawberry")
           favorite_ice_cream_flavors -= "strawberry"
         end
